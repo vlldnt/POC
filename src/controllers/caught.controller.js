a6ef8b6600ff7ee97959079ff42ad3b03a42c66d
vlldnt <vieilledent.adrien@gmail.com>
2025-08-29 11:28:16 +0200

    feat: adding pokemon caught in the guide, and swagger feat

--- src/controllers/caught.controller.js
+++ src/controllers/caught.controller.js
@@ -2,9 +2,17 @@ import { Caught } from "../models/caught.model.js";
 
 // POST : Add a new caught pokemon
 export const addPokemon = async (req, res) => {
-  const { guide_id, pokemon_id } = req.body;
-  if (!guide_id || !pokemon_id) {
+  let { guide_id, pokemon_id } = req.body;
+  if (!guide_id || pokemon_id === undefined) {
     return res.status(400).json({ error: "Guide id and pokemon id required" });
+  }
+
+  // Format pokemon_id string of 3 numbers
+  pokemon_id = pokemon_id.toString().padStart(3, "0");
+
+  // format validation
+  if (!/^[0-9]{3}$/.test(pokemon_id) || Number(pokemon_id) < 1 || Number(pokemon_id) > 151) {
+    return res.status(400).json({ error: "pokemon_id must be a string from '001' to '151'" });
   }
 
   try {
@@ -12,8 +20,11 @@ export const addPokemon = async (req, res) => {
       guide_id,
       pokemon_id,
     });
-    res.status(201).json({ message: "Pokemon caught:", caught });
+    res.status(201).json({ message: `Pokemon caught: ${pokemon_id}` });
   } catch (err) {
+    if (err.name === "SequelizeUniqueConstraintError") {
+      return res.status(400).json({ error: `Pokemon ${pokemon_id} already caught` });
+    }
     res.status(500).json({ error: err.message });
   }
 };
--- src/models/caught.model.js
+++ src/models/caught.model.js
@@ -12,7 +12,17 @@ export const Caught = sequelize.define("Caught", {
     allowNull: false,
   },
   pokemon_id: {
-    type: DataTypes.INTEGER,
+    type: DataTypes.STRING(3),
     allowNull: false,
+    validate : {
+      is: /^[0-9]{3}$/,
+    },
   },
+}, {
+  indexes: [
+    {
+      unique: true,
+      fields: ["guide_id", "pokemon_id"],
+    },
+  ],
 });
